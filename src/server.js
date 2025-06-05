3363c5ad8e616e3ce7a164b5ccd0e9189f8934c2
Alisha Zaveri <alisha.zaveri2@gmail.com>
2025-06-05 08:28:07 +0530

    initialize server with Express, dotenv, and CORS; add .env and .gitignore files

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const { config } = require("dotenv");
+
+config();
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.log(`server started at ${process.env.PORT}`);
+});
