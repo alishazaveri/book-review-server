c9e0a1fb66f896dbc5b72540398dbd78142b87da
Alisha Zaveri <alisha.zaveri2@gmail.com>
2025-06-05 09:11:38 +0530

    add user model, controller, and routes; implement user creation and MongoDB connection

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,7 @@
+async function createUser(req, res) {
+  try {
+  } catch (error) {
+    console.log("Error in Create User Controller", error);
+    return;
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const { config } = require("dotenv");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user.routes");
 
 config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+
 app.listen(process.env.PORT, () => {
   console.log(`server started at ${process.env.PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo DB connected successfully!"))
+  .catch((error) => console.log("Error connecting to Mongo DB", error));
